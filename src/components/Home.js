0e5edcf0598c5b1db77d50bff3e809e603a7f610
Marco <marcoparra311@gmail.com>
2021-11-10 19:17:22 -0400

    Install dependencies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,19 @@
 import React from 'react';
+import fetchMovies from '../services/API';
 // config
 // POSTER_SIZE
-import { BACKDROP_SIZE, IMAGE_BASE_URL } from '../services/config';
+// import { BACKDROP_SIZE, IMAGE_BASE_URL } from '../services/config';
 // Components
-import HeroImage from './HeroImage';
+// import HeroImage from './HeroImage';
 // Hook
-import useHomeFetch from './hooks/useHomeFetch';
+// import useHomeFetch from './hooks/useHomeFetch';
 // Image
 // import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
-  console.log(state, loading, error);
+  console.log(fetchMovies());
   return (
-    <>
-      {state.results[0]
-        ? (
-          <HeroImage
-            image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-            title={state.results[0].original_title}
-            text={state.results[0].overview}
-          />
-        )
-        : null}
-    </>
+    <h1>Home</h1>
   );
 };
 
--- src/services/API.js
+++ src/services/API.js
@@ -2,17 +2,13 @@ import axios from 'axios';
 import {
   SEARCH_BASE_URL,
   POPULAR_BASE_URL,
-  API_URL,
-  API_KEY,
-  REQUEST_TOKEN_URL,
-  LOGIN_URL,
-  SESSION_ID_URL,
 } from './config';
 
 const fetchMovies = async (page, searchTerm = '') => {
   const endpoint = searchTerm ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}` : `${POPULAR_BASE_URL}&page=${page}`;
-  const response  = await axios.get(endpoint);
+  const response = await axios.get(endpoint);
   const { data } = response;
-  console.log(data);
-}
+  console.log(await data);
+};
 
+export default fetchMovies;
