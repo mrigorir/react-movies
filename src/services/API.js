262fbb7ecf9f806a1b9cb1a946a8bcb622bf757f
Marco <marcoparra311@gmail.com>
2021-11-10 19:42:59 -0400

    Create fetchMovies async function

--- src/services/API.js
+++ src/services/API.js
@@ -4,11 +4,19 @@ import {
   POPULAR_BASE_URL,
 } from './config';
 
-const fetchMovies = async (page, searchTerm = '') => {
-  const endpoint = searchTerm ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}` : `${POPULAR_BASE_URL}&page=${page}`;
+const fetchMovies = async (pag, searchTerm = '') => {
+  // {page: 1, results: Array(20), total_pages: 500, total_results: 10000}
+  const endpoint = searchTerm ? `${SEARCH_BASE_URL}${searchTerm}&page=${pag}` : `${POPULAR_BASE_URL}&page=${pag}`;
   const response = await axios.get(endpoint);
   const { data } = response;
-  console.log(await data);
+  const movies = [];
+  movies.push({
+    page: data.page,
+    results: data.results,
+    totalPages: data.total_pages,
+    totalResults: data.total_results,
+  });
+  return movies;
 };
 
 export default fetchMovies;
